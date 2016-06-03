e69734ca6b7cf75b784d488f4887715789989434
Tomisha72 <ismd1@b326-s4.lab.lagcc>
2016-06-02 21:09:06 -0400

    TRY

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+var named = function(){
+  var myName = prompt("what is your name?");
+  myName.toUppercase();
+  return myName;
+}
+var myName = "Tomisha";
+console. log(myName);
+var myName = prompt("what is your name?");
+if ( myName.length> 5){
+  alert("nameis greater than 5");
+}else{
+  document.write("my name is greater than 5");
+  alert("name is less than 5");
+};
